723887e8152e46434abedb73c69765cc80624ef7
JohnDoughhh <thedoughfamilyyy@gmail.com>
2025-11-11 19:48:41 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const { Client } = require("mpp-client-net");
+
+const client = new Client("wss://mppclone.com", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMzcyNjAzYmQ4ZTFkYWQ0ZGMyMGU2NzYiLCJpYXQiOjE3NjI5MDgyMTUsImlzcyI6ImlwQG1wcGNsb25lLmNvbSJ9.0t3j_G41HcxbVOu8Sr6mnMRljmPIdGJ40CAnxRO4zgA");
+
+client.on("hi", (msg) => {
+  client.sendArray([{ m: "userset", set: { name: "[BOT] sher", color: "#adbaff" } }]);
+  client.sendArray([{ m: "a", message: "``sher bot is online!``" }]);
+}
